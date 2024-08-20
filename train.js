f45dbd30418ec483a4522a1c198aacb704d70c58
boburishmetov <boburmiro-77@mail.ru>
2024-08-20 22:37:24 +0900

    feat: train callback hamda asyc function trayn ham bor

--- train.js
+++ train.js
@@ -1,13 +1,14 @@
-console.log("Jach Ma maslahatlari");
-const list = [
-    "yaxshi tralaba boling", // 0-20
-    "togri boshliq tanlang va koproq hato qiling", //20-30
-    "uzingizga ishlashingizni boshlang", //30-40
-    "siz kuchli bolgan narsalarni qiling", //40-50
-    "yoshlarga invistitsiya qiling", //50-60
-    "endi dam oling, foydasi yoq endi", //60
-];
+// console.log("Jach Ma maslahatlari");
+// const list = [
+//     "yaxshi tralaba boling", // 0-20
+//     "togri boshliq tanlang va koproq hato qiling", //20-30
+//     "uzingizga ishlashingizni boshlang", //30-40
+//     "siz kuchli bolgan narsalarni qiling", //40-50
+//     "yoshlarga invistitsiya qiling", //50-60
+//     "endi dam oling, foydasi yoq endi", //60
+// ];
 
+// callback function
 // function maslahatBering(a, callback){
 //     if (typeof a !== "number") callback("insert a number", null);
 //     else if (a <= 20) callback(null, list[0]);
@@ -33,28 +34,28 @@ const list = [
 
 // console.log('passed here 1');
 
-
-async function maslahatBering(a){
-    if (typeof a !== "number") throw new Error("insert a number");
-    else if (a <= 20) return(null, list[0]);
-    else if (a > 20 && a <= 30) return list[1];
-    else if (a > 30 && a <= 40) return list[2];
-    else if (a > 40 && a <= 50) return list[3];
-    else if (a > 50 && a <= 60) return list[4];
-    else {
-        return new Promise((resolve, reject) => {
-            setTimeout(() => {
-                resolve(list[5]);
-            }, 5000);
-        });
+//ASYN function
+// async function maslahatBering(a){
+//     if (typeof a !== "number") throw new Error("insert a number");
+//     else if (a <= 20) return(null, list[0]);
+//     else if (a > 20 && a <= 30) return list[1];
+//     else if (a > 30 && a <= 40) return list[2];
+//     else if (a > 40 && a <= 50) return list[3];
+//     else if (a > 50 && a <= 60) return list[4];
+//     else {
+//         return new Promise((resolve, reject) => {
+//             setTimeout(() => {
+//                 resolve(list[5]);
+//             }, 5000);
+//         });
     //    setTimeout(function() {
     //     callback(null, list[5])
     //    }, 5000);
-    }
-}
+//     }
+// }
 
 
-//then/catch
+//call via then/catch
 // console.log('passed here 0');
 // maslahatBering(25)
 //  .then((data) => {
@@ -66,13 +67,38 @@ async function maslahatBering(a){
 // console.log('passed here 1');
 
 
-//async/await
-async function run() {
-    let javob = await maslahatBering(25);
-    console.log(javob);
-    javob = await maslahatBering(70);
-    console.log(javob);
-    javob = await maslahatBering(41);
-    console.log(javob);    
+// call via async/await
+// async function run() {
+//     let javob = await maslahatBering(25);
+//     console.log(javob);
+//     javob = await maslahatBering(70);
+//     console.log(javob);
+//     javob = await maslahatBering(41);
+//     console.log(javob);    
+// }
+// run();
+
+// Misol:
+
+
+/*Masalani izohi
+A-TASK: 
+
+Shunday 2 parametrli function tuzing, hamda birinchi parametrdagi letterni ikkinchi parametrdagi sozdan qatnashgan sonini return qilishi kerak boladi.
+MASALAN countLetter("e", "engineer") 3ni return qiladi.*/
+
+//Javoob:
+
+function countLetter(e, engineer) {
+    let a = 0;
+    for (let i = 0; i < engineer.length; i++) {
+        if (engineer[i] === e) {
+            a++;
+        }
+    }
+    return a;
 }
-run();
+
+
+const ATASC = countLetter("e", "engineer");
+console.log("A-TASK:", ATASC);
