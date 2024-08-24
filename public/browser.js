cd400c4f720fcdcbb8dc81cb38fd625d2ffc9453
boburishmetov <boburmiro-77@mail.ru>
2024-08-24 18:31:18 +0900

    feat:26-27 C-TASK

--- public/browser.js
+++ public/browser.js
@@ -39,3 +39,26 @@ document
         console.log("Iltimos qaytatdan harakat qiling!");        
     });
 });
+
+document.addEventListener("click", function(e) {
+    console.log(e.target);
+    
+    //delete oper
+    if(e.target.classList.contains("delete-me")) {
+        if(confirm("aniq o\`chirmoqchimisiz?")) {
+            axios.post("/delete-item", {id: e.target.getAttribute("data-id")})
+            .then((response) => {
+                console.log(response.data);
+                e.target.parentElement.parentElement.remove();
+            })
+            .catch((err) => {
+                console.log("Iltimos qaytatdan harakat qiling!");
+            });
+        }
+    }
+    
+    //edit oper
+    if(e.target.classList.contains("edit-me")) {
+        alert("siz edit tugmasini bosdingiz");
+    }
+})
